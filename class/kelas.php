841379dd5c0556ad2dbcefe6054bf5968ca09851
Gede Darma Yuda <gedheyuda398@gmail.com>
2022-12-11 10:52:27 +0700

    commit Kelas

--- class/kelas.php
+++ class/kelas.php
@@ -1,2 +1,32 @@
 <?php
+class Kelas
+{
+    private $conn;
+    private $db_table = "kelas";
+    public $kode_kelas;
+    public function __construct($db)
+    {
+        $this->conn = $db;
+    }
+    public function showKelas()
+    {
+        $sql = "SELECT * FROM " . $this->db_table . "";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute();
+        return $stmt;
+    }
+    public function deleteKelas(){
+        $sql = "DELETE FROM " . $this->db_table . " WHERE id = ?";
+        $stmt = $this->conn->prepare($sql);
+    
+        $this->kode_kelas=htmlspecialchars(strip_tags($this->kode_kelas));
+    
+        $stmt->bindParam(1, $this->kode_kelas);
+    
+        if($stmt->execute()){
+            return true;
+        }
+        return false;
+    }
+}
 ?>
